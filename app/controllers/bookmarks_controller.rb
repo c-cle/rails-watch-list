324270c2df024a919f42d8bd6eb1645e4475de2c
Claudio Clemente <claudioclemente@hotmail.it>
2022-05-26 17:04:02 +0200

    watchlist

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:movie_id])
+
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
